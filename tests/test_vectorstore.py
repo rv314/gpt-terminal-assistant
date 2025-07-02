b560305ef926d3b6d03a47d6c9e559c9ea3c7f3c
rv314 <rvp314@gmail.com>
2025-07-02 17:22:28 -0400

    fixed arg pos

--- tests/test_vectorstore.py
+++ tests/test_vectorstore.py
@@ -5,18 +5,12 @@ import asyncio
 
 def test_vectorstore_basic():
   store = VectorStore()
-  
-
-  # Add message
-  #store.add_message("I love programming in Python", "user")
-  #store.add_message("Python is awesome", "assistant")
-  #store.add_message("We are using Vlite for vector store", "user")
 
   # Add messages
   documents = ["I love programming in Python", "Python is awesome", "We are using Vlite for vector store"]
   uuids = [str(uuid4()) for _ in range(len(documents))]
 
-  store.add_documents(documents, uuids, "user")
+  store.add_documents(documents, "user", uuids)
   # Search
   results = store.search_similar("Tell me about Python", k=1)
 
@@ -39,5 +33,5 @@ def test_vectorstore_single():
 
 if __name__ == "__main__":
   print(f"Executing from {sys.executable}")
-  #test_vectorstore_basic()
-  test_vectorstore_single()
+  test_vectorstore_basic()
+  #test_vectorstore_single()
