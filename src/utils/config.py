0e712cd150c43adfde62a25b46a901b4edc4e537
rv314 <rvp314@gmail.com>
2025-07-07 23:52:31 -0400

    load configuration

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,6 +3,6 @@ import os
 
 def load_config():
   base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-  config_path = os.path.join(base_dir, "config.json")
+  config_path = os.path.join(base_dir, "../config.json")
   with open(config_path, "r") as f:
     return json.load(f)
